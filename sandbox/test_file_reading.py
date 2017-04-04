13c4233eee181ae3914decb92761682bc7bb2409
Matthew Bellis <matthew.bellis@gmail.com>
2017-04-04 01:47:26 -0400

    Added first-order working version of file reading.

---
+++ sandbox/test_file_reading.py
@@ -0,0 +1,37 @@
+import h5py as h5
+import numpy as np
+
+import matplotlib.pylab as plt
+
+import time
+
+from hep_hdf5_tools import hd5events,get_event
+
+import sys
+
+filename = sys.argv[1]
+
+data,event = hd5events(filename,verbose=True)
+
+nevents = 100000
+
+energies = []
+for i in range(0,nevents):
+
+    if i%1000==0:
+        print(i)
+
+    get_event(event,data,n=i)
+
+    energy = event['Jets/Energy']
+
+    for e in energy:
+        energies.append(e)
+
+
+
+
+
+
+
+
